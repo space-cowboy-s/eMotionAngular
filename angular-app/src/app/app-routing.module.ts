d6ffe356f6960d0a940e22f075d8572e9e6ff3e1
Genoudj <genoud.jeremy@outlook.com>
2019-07-28 12:46:19 +0200

    Add route and edit index.html content

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomepageComponent} from './homepage/homepage.component';
+import {AddcarComponent} from './component/addcar/addcar.component';
+import {CarsdispoComponent} from './component/carsdispo/carsdispo.component';
+import {ConditionComponent} from './component/condition/condition.component';
+import {ContratComponent} from './component/contrat/contrat.component';
+import {FactureComponent} from './component/facture/facture.component';
+import {HistoricComponent} from './component/historic/historic.component';
+import {InfouserComponent} from './component/infouser/infouser.component';
+import {ListcarsComponent} from './component/listcars/listcars.component';
+import {ListcommandComponent} from './component/listcommand/listcommand.component';
+import {ListuserComponent} from './component/listuser/listuser.component';
+import {ProfiluserComponent} from './component/profiluser/profiluser.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , component: HomepageComponent},
+  {path: 'addcar', component: AddcarComponent},
+  {path: 'carsdispo', component: CarsdispoComponent},
+  {path: 'condition', component: ConditionComponent},
+  {path: 'contrat', component: ContratComponent},
+  {path: 'facture', component: FactureComponent},
+  {path: 'historic', component: HistoricComponent},
+  {path: 'infouser', component: InfouserComponent},
+  {path: 'listcars', component: ListcarsComponent},
+  {path: 'listcommand', component: ListcommandComponent},
+  {path: 'listuser', component: ListuserComponent},
+  {path: 'profiluser', component: ProfiluserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
